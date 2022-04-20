b6b718b021a9307b6c7e8714eb47d815aca69681
Christy Park <christypark21@gmail.com>
2022-04-19 21:06:40 -0500

    failed front-end build

--- src/index.js
+++ src/index.js
@@ -6,27 +6,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import * as serviceWorker from './serviceWorker';
-import Landing from './pages/Landing'
 import './styles/style.css';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-import { ChakraProvider, theme, CSSReset } from '@chakra-ui/react';
-
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 <StrictMode>
-<ChakraProvider theme={theme}>
-
     <BrowserRouter>
       <ColorModeScript />
       <Routes>
-      <Route path="/landing" element={<Landing />} />
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
-    </ChakraProvider>
     ,
   </StrictMode>
   );
